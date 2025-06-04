2b6c306055e31857d66815d390db41b404b8d996
Aaron Myles Landwehr <snaphat@gmail.com>
2025-06-04 03:33:48 -0400

    Remove refs/head

--- google-docs-image-rendering-fallback.user.js
+++ google-docs-image-rendering-fallback.user.js
@@ -2,7 +2,7 @@
 // @name         Google Docs Image Rendering Fallback
 // @description  Spoofs the Firefox user agent to make Google Docs fall back to non-canvas image rendering while preserving canvas rendering for text.
 // @version      0.0.1
-// @icon         https://raw.githubusercontent.com/snaphat/google-docs-image-rendering-fallback/refs/heads/main/Setting-Sun-Lantern-From-Papierlaternen-Fabrik-Riethmueller-Icon.png
+// @icon         https://raw.githubusercontent.com/snaphat/google-docs-image-rendering-fallback/main/Setting-Sun-Lantern-From-Papierlaternen-Fabrik-Riethmueller-Icon.png
 // @match        https://docs.google.com/*
 // @run-at       document-start
 // @grant        none
